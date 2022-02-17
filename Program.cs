e5760a6ff235d4d9dae053237a10c791cbdc75ba
Looken15 <tema-super-man@yandex.ru>
2022-02-17 21:58:18 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZMQServer
+{
+    class Program
+    {
+        public static string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public static string runtimePath = appDataPath + "\\jupyter\\runtime\\";
+        static void Main(string[] args)
+        {
+            var connectionFilePath = Directory.GetFiles(runtimePath, "kernel*")
+                                    .Select(x => new { path = x, creationTime = File.GetCreationTime(x) })
+                                    .OrderByDescending(x => x.creationTime)
+                                    .First()
+                                    .path;
+
+            var jsonConnectionFile = File.ReadAllText(connectionFilePath);
+
+            var currentConnection = (connection)JsonSerializer.Deserialize(jsonConnectionFile, typeof(connection));
+
+            File.AppendAllText("C:\\Users\\Tema-\\Desktop\\ZMQExample\\ZMQServer\\log.txt", currentConnection.hb_port.ToString());
+
+            using (var socket = new ResponseSocket())
+            {
+                var address = $"{currentConnection.transport}://{currentConnection.ip}:{currentConnection.hb_port}";
+                socket.Connect(address);
+
+                while (true)
+                {
+                    File.AppendAllText("C:\\Users\\Tema-\\Desktop\\ZMQExample\\ZMQServer\\log.txt", "receiving\n");
+                    var data = socket.ReceiveFrameBytes();
+                    File.AppendAllText("C:\\Users\\Tema-\\Desktop\\ZMQExample\\ZMQServer\\log.txt", Convert.ToBase64String(data) + "\n");
+                    socket.SendFrame(data);
+                }
+            }
+        }
+    }
+}
---
+++ connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMQServer
+{
+    public class connection
+    {
+        public int shell_port { get; set; }
+        public int iopub_port { get; set; }
+        public int stdin_port { get; set; }
+        public int control_port { get; set; }
+        public int hb_port { get; set; }
+        public string ip { get; set; }
+        public string key { get; set; }
+        public string transport { get; set; }
+        public string signature_scheme { get; set; }
+        public string kernel_name { get; set; }
+    }
+
+}
